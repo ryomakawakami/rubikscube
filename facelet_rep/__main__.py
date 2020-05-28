57c7bf13d2c185c58f2c66639246a888666d6b5f
ryomakawakami <ryomakwkm@gmail.com>
2020-05-27 21:28:21 -0400

    Facelet rep separated, start work on cubie rep

---
+++ facelet_rep/__main__.py
@@ -0,0 +1,8 @@
+import rubikscube.cube.cube as cube
+
+if __name__ == '__main__':
+    print("Cube")
+
+    cube = cube.Cube()
+    cube.handScramble("x R2' F R F' R U2 r' U r U2 x'")
+    cube.display()
